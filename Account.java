901fa664187ddf96cc3e21cf14bbfbfcbd84c765
aescorcia05 <156947111+aescorcia05@users.noreply.github.com>
2024-03-12 10:13:54 -0400

    Completed Account.java

--- Account.java
+++ Account.java
@@ -1,5 +1,3 @@
-import java.net.SocketOption;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Account {
@@ -7,29 +5,42 @@ public class Account {
     // Attributes
 
     // name - name of the account holder
-    String name;
+    private String name;
     // lastName - last name of the account holder
-    String lastName;
+    private String lastName;
     // password - password for the account
-    int pin;
+    private int pin;
     // last5Transactions - stores the last 5 transactions (positive for deposits and negative for withdrawals)
-    double[] last5Transactions; // most recent first
+    private double[] last5Transactions; // most recent first
     // balance - current balance of the account
-    double balance;
+    private double balance;
+    private boolean blocked;
 
     //Constructor:
-    Account(String name, String lastName) {
+    public Account(String name, String lastName) {
 
         this.name = name;
         this.lastName = lastName;
-        this.pin = 1;
+        this.setPin();
         this.balance = 0.00;
         this.last5Transactions = new double[5];
+        this.blocked = false;
+
+    }
+
+    protected Account(String name, String lastName, int pin, double balance, double[] last5Transactions) {
+
+        this.name = name;
+        this.lastName = lastName;
+        this.pin = pin;
+        this.balance = balance;
+        this.last5Transactions = last5Transactions;
+        this.blocked = false;
 
     }
 
     // encryptPassword - encrypts a String
-    static int encryptPassword(int pin) {
+    protected static int encryptPassword(int pin) {
         int g = 5;
         int p = 10007;
         int publicKey = 1;
@@ -60,7 +71,6 @@ public class Account {
 
                 System.out.print("Please input your pin: ");
                 numStr = input.next();
-                //System.out.print("\n");
                 try {num = Integer.parseInt(numStr); invalid = false;}
                 catch(Exception NotAnInt) {invalid = true; System.out.print("Invalid pin. ");}
 
@@ -75,7 +85,7 @@ public class Account {
     }
 
     // getNewPassword - gets an encrypted password and stores it
-    void getNewPassword() {
+    protected void getNewPassword() {
 
         int pin1 = 1;
         int pin2 = 0;
@@ -92,30 +102,33 @@ public class Account {
     }
 
     // isOwner(userKey) - determines if userKey matches the stored encrypted key
-    boolean isOwner(int userKey) {
-        return Account.encryptPassword(userKey) == pin;
+    protected boolean isOwner(int userKey) {
+        blocked = blocked || Account.encryptPassword(userKey) != pin;
+        return !blocked;
     }
 
     // void deposit(amount) - updates balance after the user deposits an amount
-    void deposit(double amount) {
+    protected void deposit(double amount) {
         balance  += amount;
-        for(int i = 0; i < last5Transactions.length - 1; i++) {
-            last5Transactions[i+1] = last5Transactions[i];
-        }
+        last5Transactions[4] = last5Transactions[3];
+        last5Transactions[3] = last5Transactions[2];
+        last5Transactions[2] = last5Transactions[1];
+        last5Transactions[1] = last5Transactions[0];
         last5Transactions[0] = amount;
     }
 
     //void withdraw(amount) - updates balance after the user withdraws an amount
-    void withdraw(double amount) {
+    protected void withdraw(double amount) {
         balance -= amount;
-        for(int i = 0; i < last5Transactions.length - 1; i++) {
-            last5Transactions[i+1] = last5Transactions[i];
-        }
+        last5Transactions[4] = last5Transactions[3];
+        last5Transactions[3] = last5Transactions[2];
+        last5Transactions[2] = last5Transactions[1];
+        last5Transactions[1] = last5Transactions[0];
         last5Transactions[0] = -amount;
     }
 
     // String[] statsToStrings() - returns the summary of recent account activities as an array: min, max, average, currentBalance
-    String[] statsToStrings() {
+    protected String[] statsToStrings() {
         double min = last5Transactions[0];
         double max = last5Transactions[0];
         double average = 0.0;
@@ -137,16 +150,198 @@ public class Account {
         }
         average /= last5Transactions.length;
 
-        returnArray[0] = String.valueOf(min);
-        returnArray[1] = String.valueOf(max);
-        returnArray[2] = String.valueOf(average);
-        returnArray[3] = String.valueOf(balance);
+        returnArray[0] = "Minimum transaction: " + min;
+        returnArray[1] = "Maximum transaction: " + max;
+        returnArray[2] = "Average transaction: " + average;
+        returnArray[3] = "Current balance: " + balance;
 
         return returnArray;
 
     }
 
-    // void displayStats() - prints the account info
-    // NEED TO CODE
+    // displayStats() - prints the account info
+    protected void displayStats() {
+        double min = last5Transactions[0];
+        double max = last5Transactions[0];
+        double average = 0.0;
+
+        // min
+        for(int i = 1; i < last5Transactions.length; i++) {
+            if(last5Transactions[i] < min) {min = last5Transactions[i];}
+        }
+
+        // max
+        for(int i = 1; i < last5Transactions.length; i++) {
+            if(last5Transactions[i] > max) {max = last5Transactions[i];}
+        }
+
+        // average
+        for(int i = 0; i < last5Transactions.length - 1; i++) {
+            average += last5Transactions[i];
+        }
+        average /= last5Transactions.length;
+
+        System.out.println("Minimum transaction: " + min);
+        System.out.println("Maximum transaction: " + max);
+        System.out.println("Average transaction: " + average);
+        System.out.println("Current balance: " + balance);
+    }
+
+    // toFile - returns a String with the information of the account to save to a file
+    public String toFile() {
+        return name + " " + lastName + " " + pin + " " + balance + " " + last5Transactions[0] + " " + last5Transactions[1] + " " + last5Transactions[2] + " " + last5Transactions[3] + " " + last5Transactions[4];
+    }
+
+    public char getTransaction() {
+
+        Scanner input = new Scanner(System.in);
+
+        // Creating list of options for user input
+        char[] transactionOptions = {'D', 'W', 'C', 'S', 'V', 'E'};
+
+        // Outputting options to the user
+        System.out.println("Hi " + name + ", what can this AEBank's ATM do for you today?\n");
+        System.out.println("  D eposit");
+        System.out.println("  W ithdraw");
+        System.out.println("  C heck balance");
+        System.out.println("  S how account statistics");
+        System.out.println("  V iew recent transactions");
+        System.out.println("  E xit");
+        System.out.println();
+
+        // Initializing variables
+        String transactionStr;
+        char transaction;
+        boolean validInput = true;
+
+        // Getting input
+        transactionStr = input.next();
+
+        // User can only opt for hacker mode the first time.
+        if (transactionStr.equals("H4CK3R")) {return 'H';}
+
+        // Turning input to char type
+        transaction = transactionStr.toUpperCase().charAt(0);
+
+        // Checking input
+        for(int i = 0; i < transactionOptions.length; i++) {validInput = validInput && (transaction != transactionOptions[i]);}
+
+        // Prompt again while input is invalid
+        while (validInput) {
+
+            // Prompting the user again
+            System.out.println("Please select one of the following options:\n");
+            System.out.println("  D eposit");
+            System.out.println("  W ithdraw");
+            System.out.println("  C heck balance");
+            System.out.println("  S how account statistics");
+            System.out.println("  V iew recent transactions");
+            System.out.println("  E xit");
+            System.out.println();
+
+            // Turning input to char type
+            transaction = input.next().toUpperCase().charAt(0);
+
+            // Checking input
+            validInput = true;
+            for(int i = 0; i < transactionOptions.length; i++) {validInput = validInput && (transaction != transactionOptions[i]);}
+        }
+
+        return transaction;
+
+    }
+
+    protected void displayLast5Transactions() {
+        for (int i = 0; i < 5; i++) {
+            if (last5Transactions[i] != 0) {
+                System.out.println("Transaction " + (i + 1) + ": " + last5Transactions[i]);
+            }
+        }
+    }
+
+    private void setPin() {
+        Scanner input = new Scanner(System.in);
+
+        int num1;
+        int num2;
+        String numStr;
+        boolean invalid;
+
+        num1 = -1;
+        num2 = 0;
+
+        while (num1 != num2) {
+
+            num1 = -1;
+            num2 = 0;
+
+            while (num1 < 2 || num1 > 9999) {
+
+                invalid = true;
+
+                while (invalid) {
+
+                    System.out.print("Please input your pin: ");
+                    numStr = input.next();
+                    //System.out.print("\n");
+                    try {
+                        num1 = Integer.parseInt(numStr);
+                        invalid = false;
+                    } catch (Exception NotAnInt) {
+                        invalid = true;
+                        System.out.print("Invalid pin. ");
+                    }
+
+                }
+
+                if (num1 < 2 || num1 > 9999) {
+                    System.out.print("Invalid pin. ");
+                }
+
+            }
+
+            while (num2 < 2 || num2 > 9999) {
+
+                invalid = true;
+
+                while (invalid) {
+
+                    System.out.print("Please input your pin again: ");
+                    numStr = input.next();
+                    //System.out.print("\n");
+                    try {
+                        num2 = Integer.parseInt(numStr);
+                        invalid = false;
+                    } catch (Exception NotAnInt) {
+                        invalid = true;
+                        System.out.print("Invalid pin. ");
+                    }
+
+                }
+
+                if (num2 < 2 || num2 > 9999) {
+                    System.out.print("Invalid pin. ");
+                }
+
+            }
+
+            if (num1 != num2) {
+                System.out.print("Pins don't match, try again!");
+            }
+
+        }
+
+        pin = encryptPassword(num1);
+
+    }
+
+    protected void setBalance(double newBalance) {balance = newBalance;}
+    protected void unblock() {blocked = false;}
+
+    public double balance() {return balance;}
+    public String name() {return name;}
+    public String lastName() {return lastName;}
+    protected int pin() {return pin;}
+    protected double[] last5Transactions() {return last5Transactions;}
 
 }
---
+++ Main.java
@@ -0,0 +1,134 @@
+import java.net.SocketOption;
+import java.util.Scanner;
+
+public class Main {
+
+    public static double getPositiveNum(String prompt) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Initializing variables
+        double num = -1.0;
+        String numStr;
+        boolean invalid;
+
+        // Prompt again while input is invalid
+        while (num <= 0.0) {
+
+            // [Re]setting invalid to true
+            invalid = true;
+
+            while (invalid) {
+                // Prompting user
+                System.out.println(prompt);
+                // Getting input
+                numStr = input.next();
+                // Trying to convert input
+                try {num = Double.parseDouble(numStr); invalid = false;}
+                // If the conversion fails then the input is invalid and the user is prompted again
+                catch(Exception NotADouble) {invalid = true;}
+            }
+        }
+        // Returning input
+        return num;
+    }
+    public static char wantNewAccount(String name) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Creating list of options for user input
+        char[] options = {'Y', 'N'};
+
+        // Outputting options to the user
+        System.out.println("Do you want to create a new one " + name + "?");
+
+        // Initializing variables
+        char choice;
+        boolean validInput = true;
+
+        // Turning input to char type
+        choice = input.next().toUpperCase().charAt(0);
+
+        // Checking input
+        for(int i = 0; i < options.length; i++) {validInput = validInput && (choice != options[i]);}
+
+        // Prompt again while input is invalid
+        while (validInput) {
+
+            // Prompting the user again
+            System.out.println("Do you want to create a new one " + name + "?");
+
+            // Turning input to char type
+            choice = input.next().toUpperCase().charAt(0);
+
+            // Checking input
+            validInput = true;
+            for(int i = 0; i < options.length; i++) {validInput = validInput && (choice != options[i]);}
+
+        }
+
+        return choice;
+
+    }
+    public static void headline() {
+        System.out.println("------------------------------ $ ------------------------------");
+        System.out.println("                          AE Bank ATM                          ");
+        System.out.println("------------------------------ $ ------------------------------");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 80; i++) {System.out.println();}
+
+        Scanner input = new Scanner(System.in);
+        ATM Atm = new ATM();
+
+        boolean quit = false;
+
+        while (!quit) {
+
+            headline();
+            System.out.println("Welcome to AEBank's ATM");
+
+            System.out.println("What is your first name? ");
+            String userName = input.next();
+            System.out.println("What is your last name? ");
+            String userLastName = input.next();
+
+            if (userName.equals("4DM1N") && userLastName.equals("157R470R")) {Atm.adminMenu();} else {
+                if (userName.equals("Quit") && userLastName.equals("Quit")) {
+                    quit = true;
+                } else {
+
+                    int accountNum = Atm.namesToAccountNum(userName, userLastName);
+
+                    if (accountNum == -1) {
+                        System.out.print("No account was found under your name");
+                        if (Atm.hasSpace()) {
+                            System.out.print(", but you can set one up right now.\n");
+                            if(wantNewAccount(userName) == 'Y') {
+                                System.out.println("A new account is being created for you.");
+                                Atm.menu(Atm.createNewAccount(userName, userLastName));
+                            }
+                        } else {
+                            System.out.print(", and no space is available at the moment.");
+                        }
+                    } else {
+                        Atm.menu(accountNum);
+                    }
+
+                    for (int i = 0; i < 80; i++) {System.out.println();}
+
+                }
+            }
+
+            // Atm.saveAccounts();
+
+        }
+
+        Atm.saveAccounts();
+
+    } // end of main method
+
+}
